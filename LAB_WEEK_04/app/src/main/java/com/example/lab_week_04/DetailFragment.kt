680634dbe12150c3f039fa31459d56341364a721
JessenVallensio <jessenfu.f5@gmail.com>
2024-09-23 11:08:53 +0700

    Update DetailFragment.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/DetailFragment.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/DetailFragment.kt
@@ -25,7 +25,10 @@ class DetailFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    )
 
     companion object {
         /**
