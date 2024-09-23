8a7d7046b3db9c167e1a3c9518ab56205e6a3db3
JessenVallensio <jessenfu.f5@gmail.com>
2024-09-23 11:56:10 +0700

    Update MainActivity.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -10,14 +10,7 @@ import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
