0fe1904b82df620ab2657b13bb33f1d39cf78973
JessenVallensio <jessenfu.f5@gmail.com>
2024-09-23 11:01:23 +0700

    Update ListFragment.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/ListFragment.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/ListFragment.kt
@@ -22,6 +22,7 @@ class ListFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
