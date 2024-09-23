ab46e63e9d56bf5eb10fb60df791db78e6a9bd90
JessenVallensio <jessenfu.f5@gmail.com>
2024-09-23 11:58:16 +0700

    Update MainActivity.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -31,3 +31,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) ||
+                super.onSupportNavigateUp()
+    }
+}
