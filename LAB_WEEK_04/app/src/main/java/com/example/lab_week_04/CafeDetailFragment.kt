4acf1bfcee781a48dc995e779536597d17da8064
JessenVallensio <jessenfu.f5@gmail.com>
2024-09-29 13:02:56 +0700

    Update CafeDetailFragment.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -17,12 +17,10 @@ class CafeDetailFragment : Fragment() {
         }
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cafe_detail2, container, false)
-    }
 
     companion object {
         /**
