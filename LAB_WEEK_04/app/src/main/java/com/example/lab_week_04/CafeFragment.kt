059af414905583c067be1f1736b9ae435e3ce456
JessenVallensio <jessenfu.f5@gmail.com>
2024-09-29 12:42:56 +0700

    Update CafeFragment.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeFragment.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
